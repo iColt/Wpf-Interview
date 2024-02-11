dbc8f253312db2196f3db3de966b4b2af2461ede
iColt <eduard.kobzik@gmail.com>
2024-02-11 17:28:21 +0100

    Initial commit

---
+++ WPF.Ko/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF.Ko
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
