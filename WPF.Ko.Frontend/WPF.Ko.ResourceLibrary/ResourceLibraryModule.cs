126d54a6698a71a5f7cd459d8e76ba4f4624185f
iColt <eduard.kobzik@gmail.com>
2024-02-18 22:53:40 +0100

    Styles and resource libraries added

---
+++ WPF.Ko.Frontend/WPF.Ko.ResourceLibrary/ResourceLibraryModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace WPF.Ko.ResourceLibrary
+{
+    public class ResourceLibraryModule
+    {
+        public ResourceLibraryModule()
+        {
+            RegisterResources();
+        }
+
+        private static void RegisterResources()
+        {
+            ResourceDictionary dictionary = new();
+            dictionary.Source = new Uri("/WPF.Ko.ResourceLibrary;component/Themes/Light/LightThemeBase.xaml", UriKind.RelativeOrAbsolute);
+            Application.Current.Resources.MergedDictionaries.Add(dictionary);
+        }
+    }
+}
--- WPF.Ko/MainWindowViewModel.cs
+++ WPF.Ko/MainWindowViewModel.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using WPF.Ko.Common.Models;
 
 namespace WPF.Ko;
 public class MainWindowViewModel : INotifyPropertyChanged
 {
+    private List<string> _themes;
+
+    public List<string> Themes
+    {
+        get { return _themes; }
+        set { _themes = value; }
+    }
+
+    private string _selectedTheme;
+
+    public string SelectedTheme
+    {
+        get { return _selectedTheme; }
+        set
+        {
+            _selectedTheme = value;
+            ChangeTheme();
+        }
+    }
+
+
     private Alcohol selectedAlcohol = new();
 
     public ObservableCollection<Alcohol> Alcohols { get; set; } = new ObservableCollection<Alcohol>();
@@ -21,13 +43,26 @@ public class MainWindowViewModel : INotifyPropertyChanged
 
     public MainWindowViewModel()
     {
-        Alcohols = new ObservableCollection<Alcohol>
-            {
+        Alcohols =
+            [
                 new Alcohol { Id = 1, Type = AlcoholType.EmptyAlcoholType, AveragePrice = 0 },
                 new Alcohol { Id = 2, Type = AlcoholType.WhiskyType, AveragePrice = 200 },
                 new Alcohol { Id = 3, Type = AlcoholType.WhiskeyType, AveragePrice = 100 },
                 new Alcohol { Id = 4, Type = AlcoholType.CognacType, AveragePrice = 150}
-            };
+            ];
+        _themes = ["Light", "Dark"];
+        _selectedTheme = Themes.First();
+    }
+
+    private void ChangeTheme()
+    {
+        ResourceDictionary dictionary = new();
+        var uri = _selectedTheme.Equals("Dark") ?
+            "/WPF.Ko.ResourceLibrary;component/Themes/Dark/DarkThemeBase.xaml"
+            : "/WPF.Ko.ResourceLibrary;component/Themes/Light/LightThemeBase.xaml";
+        dictionary.Source = new Uri(uri, UriKind.RelativeOrAbsolute);
+        Application.Current.Resources.Clear();
+        Application.Current.Resources.MergedDictionaries.Add(dictionary);
     }
 
     public event PropertyChangedEventHandler PropertyChanged;
