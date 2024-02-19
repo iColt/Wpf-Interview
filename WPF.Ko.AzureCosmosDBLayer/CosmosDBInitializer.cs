1b210d8b4849b6b04efac67bc9e946f0c1ca5de7
iColt <eduard.kobzik@gmail.com>
2024-02-19 18:41:59 +0100

    CosmosDB-NoSql. Work in progress

---
+++ WPF.Ko.AzureCosmosDBLayer/CosmosDBInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF.Ko.AzureCosmosDBLayer.Helpers;
+
+namespace WPF.Ko.AzureCosmosDBLayer;
+
+public class CosmosDBInitializer
+{
+    public async Task CreateEntryAsync(string[] args)
+    {
+        var command = new RootCommand();
+
+        var nameOption = new Option<string>("--name") { IsRequired = true };
+        var emailOption = new Option<string>("--email");
+        var stateOption = new Option<string>("--state") { IsRequired = true };
+        var countryOption = new Option<string>("--country") { IsRequired = true };
+
+        command.AddOption(nameOption);
+        command.AddOption(emailOption);
+        command.AddOption(stateOption);
+        command.AddOption(countryOption);
+
+        command.SetHandler(
+            handle: CosmosHandler.ManageCustomerAsync,
+            nameOption,
+            emailOption,
+            stateOption,
+            countryOption
+        );
+
+        await command.InvokeAsync(args);
+    }
+}
---
+++ WPF.Ko.AzureCosmosDBLayer/Helpers/CosmosHandler.cs
@@ -0,0 +1,43 @@
+﻿using Humanizer;
+using Microsoft.Azure.Cosmos;
+
+namespace WPF.Ko.AzureCosmosDBLayer.Helpers;
+
+public static class CosmosHandler
+{
+    private static readonly CosmosClient _client;
+
+    static CosmosHandler()
+    {
+        //TODO: read from file
+        _client = new CosmosClient(
+           accountEndpoint: "<uri>",
+           authKeyOrResourceToken: "<primary-key>"
+       );
+    }
+
+    private static async Task<Container> GetContainer()
+    {
+        //TODO: read DB name from file
+        Database database = _client.GetDatabase("DB-NAME");
+        List<string> keyPaths = new()
+        {
+            "/address/country",
+            "/address/state"
+        };
+        ContainerProperties properties = new(
+            id: "customers",
+            partitionKeyPaths: keyPaths
+        );
+
+        return await database.CreateContainerIfNotExistsAsync(
+            containerProperties: properties
+        );
+    }
+
+    public static async Task ManageCustomerAsync(string name, string email, string state, string country)
+    {
+        //TODO: write to output
+        //await Console.Out.WriteLineAsync($"Hello {name} of {state}, {country}!");
+    }
+}
